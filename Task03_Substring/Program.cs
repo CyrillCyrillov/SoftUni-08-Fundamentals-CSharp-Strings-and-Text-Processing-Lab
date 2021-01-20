f7ccf003442482e308fc3659be3f09ca34f7954d
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-20 21:27:04 +0200

    Add files via upload

---
+++ Task03_Substring/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task03_Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textToRemoved = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            while(text.IndexOf(textToRemoved) >=0)
+            {
+                int index = text.IndexOf(textToRemoved);
+
+                text = text.Remove(index, textToRemoved.Length);
+            }
+            
+            Console.WriteLine(text);
+        }
+    }
+}
