a2754a20cbe98464b73cc960c4cb9557ba80d2c4
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-20 21:25:44 +0200

    Add files via upload

---
+++ Task02_Repeat_Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Task02_Repeat_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputWords = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string repeatWord = string.Empty;
+
+            foreach (var nextWord in inputWords)
+            {
+                for (int i = 0; i < nextWord.Length; i++)
+                {
+                    repeatWord += nextWord;
+                }
+            }
+            
+            
+            Console.WriteLine(repeatWord);
+        }
+    }
+}
