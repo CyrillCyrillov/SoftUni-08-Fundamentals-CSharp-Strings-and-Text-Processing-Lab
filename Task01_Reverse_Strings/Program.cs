d958d547523ea4ed534cfe5d5965982cff3b9333
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-20 21:24:47 +0200

    Add files via upload

---
+++ Task01_Reverse_Strings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task01_Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputWord = string.Empty;
+            
+            while (true)
+            {
+                inputWord = Console.ReadLine();
+
+                if(inputWord.ToUpper() == "END")
+                {
+                    break;
+                }
+
+                string reverseWord = string.Empty;
+
+                for (int i = inputWord.Length - 1; i >= 0; i--)
+                {
+                    reverseWord = reverseWord + inputWord[i];
+                }
+
+                Console.WriteLine($"{inputWord} = {reverseWord}");
+
+            }
+        }
+    }
+}
