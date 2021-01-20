9cf63d351906d74aae4a7058b6911c2dd3416a07
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-20 21:28:17 +0200

    Add files via upload

---
+++ Task04_Text_Filter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Task04_Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banedWords = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string text = Console.ReadLine();
+
+            foreach (var ban in banedWords)
+            {
+                string newWord = string.Empty;
+                for (int i = 1; i <= ban.Length; i++)
+                {
+                    newWord += "*";
+                }
+
+                text = text.Replace(ban, newWord);
+            }
+            
+            Console.WriteLine(text);
+        }
+    }
+}
