d736e21af52fe579682cb32a0b7fa456242a1bb7
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-20 21:29:15 +0200

    Add files via upload

---
+++ Task05_Digits,_Letters_and_Other/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+
+namespace Task05_Digits__Letters_and_Other
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string digits = string.Empty;
+            string letters = string.Empty;
+            string others = string.Empty;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if(char.IsDigit(text[i]))
+                {
+                    digits += text[i];
+                }
+
+                else if(char.IsLetter(text[i]))
+                {
+                    letters += text[i];
+                }
+
+                else
+                {
+                    others += text[i];
+                }
+            }
+            
+            
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(others);
+        }
+    }
+}
